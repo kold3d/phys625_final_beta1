3fee445563222bd5b8997b849dfbe53b5c6440a8
Sriteja Upadhyayula <kold3d@gmail.com>
2015-12-15 12:34:37 -0600

    fixed errors. about to start fitting.

--- beta1.py
+++ beta1.py
@@ -24,7 +24,7 @@ momentum = []
 eta = []
 
 totalEnergy = np.array(energy+restMass)
-momentum = np.array(np.sqrt(energy*2.*restMass))
+momentum = np.array(np.sqrt(energy * (energy + 2 * restMass)))
 eta = np.array((2*np.pi*fine*totalEnergy)/momentum)
 
 def f(zp, b):                                               #for b 0 = + and 1 = -
@@ -32,26 +32,29 @@ def f(zp, b):                                               #for b 0 = + and 1 =
     f = ((-1)**b * zp * 2 * np.pi * fine * totalEnergy/momentum)/(np.exp((-1)**b * zp * 2 * np.pi * fine * totalEnergy/momentum)-1)
     return f
 
-y1 = np.array(np.sqrt(counts/(momentum**2 * f(21,0))))
-y2 = np.array(np.sqrt(counts/(momentum**2 * f(19,1))))
 
-yerr = []
+y1 = np.array(np.sqrt(counts / (momentum ** 2 * f(21, 1))))
+y2 = np.array(np.sqrt(counts / (momentum ** 2 * f(19, 0))))
 
-def error(y):
-    for value in y:
-        if value > 0.:
-            yerr.append(1/np.sqrt(value))
-        else:
-            yerr.append(value)
-    return yerr
+# def error(y):
+#     yerr = []
+#     for value in y:
+#         if value > 0.:
+#             yerr.append(1/np.sqrt(value))
+#         else:
+#             yerr.append(value)
+#     return yerr
+#
+# print max(error(counts))
 
-print min(error(counts))
+yerr1 = 1 / (2 * momentum * np.sqrt(f(21, 1)))
+yerr2 = 1 / (2 * momentum * np.sqrt(f(19, 0)))
 
 
 fig, ax = plt.subplots()
 # pt0 = ax.errorbar(energy,np.sqrt(counts/(((-1)**0 * 19 * eta)/(np.exp((-1)**0 * 19 * eta)-1)*momentum*momentum)), yerr=yerr, fmt='.', ecolor='g', capthick=.2)
 
-# pt0 = ax.errorbar(energy,y2, yerr=error(counts), fmt='.', ecolor='g', capthick=.2)
+pt0 = ax.errorbar(energy, y2, yerr=yerr2, fmt='.', ecolor='g', capthick=.2)
 #pt1 = ax.errorbar(energy, y1, yerr=yerr, fmt='.', ecolor='g', capthick=.2)
 
 
